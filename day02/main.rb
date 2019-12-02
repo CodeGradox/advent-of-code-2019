8bf2801b357626d530ee7c8464012045378c1092
Magnus Åsrud <magnus.asrud@newgencom.com>
2019-12-02 18:23:10 +0100

    Prettify solution. :)

--- day02/main.rb
+++ day02/main.rb
@@ -3,32 +3,41 @@ program = File.open("input").read.split(",").map(&:to_i)
 # program = [1,9,10,3,2,3,11,0,99,30,40,50]
 # program = [1,1,1,4,99,5,6,0,99]
 
-# Part 1
+ADD = 1
+MUL = 2
+HALT = 99
+
+def apply_intcode(program, idx, &opt)
+  a, b, c = program[idx + 1, idx + 3]
+  program[c] = opt.call(program[a], program[b])
+end
+
 def real_intcode_computer(program, noun, verb)
+  program = program.clone
   program[1] = noun
   program[2] = verb
   idx = 0
-  while program[idx] != 99
-    if program[idx] == 1
-      a, b, c = program[idx + 1, idx + 3]
-      program[c] = program[a] + program[b]
-    elsif program[idx] == 2
-      a, b, c = program[idx + 1, idx + 3]
-      program[c] = program[a] * program[b]
+  loop do
+    case program[idx]
+    when ADD
+      apply_intcode(program, idx, &:+)
+    when MUL
+      apply_intcode(program, idx, &:*)
+    when HALT
+      return program[0]
     else
       return nil
     end
     idx += 4
   end
-  return program[0]
 end
 
 puts "Part 1"
-puts real_intcode_computer(program.clone, 12, 2)
+puts real_intcode_computer(program, 12, 2)
 
 (0..99).each do |noun|
   (0..99).each do |verb|
-    ret = real_intcode_computer(program.clone, noun, verb)
+    ret = real_intcode_computer(program, noun, verb)
     next if ret.nil?
     if ret == 19690720
       puts "Part 2"
